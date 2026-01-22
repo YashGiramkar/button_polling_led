2156bb230cbbeb41106a98b70c262c3856126294
Yash Giramkar <yashgiramkar007@gmail.com>
2026-01-23 00:26:43 +0530

    Modified: - main.c to now include the logic for user button polling and LED status update.
    Signed-off-by: Yash Giramkar <yashgiramkar007@gmail.com>

--- src/main.c
+++ src/main.c
@@ -2,24 +2,43 @@
 #include <zephyr/drivers/gpio.h>
 
 #define SLEEP_TIME_MS 1000
+
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define USER_BUTTON_NODE DT_ALIAS(sw0)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(USER_BUTTON_NODE, gpios);
 
-int  main(void)
+int main(void)
 {
+   int ret = 0;
    // Check if the GPIO device is ready
-   if (!gpio_is_ready_dt(&led))
+   if (!gpio_is_ready_dt(&led0) or !gpio_is_ready_dt(&led1) or gpio_is_ready_dt(&button))
    {
-         return 0;
+         return -1;
    }
 
-   gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+   ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+   ret |= gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+   ret |= gpio_pin_configure_dt(&button, GPIO_INPUT);
+
+   if(ret<0)
+   {
+      return -1;
+   }
 
    while (1)
    {
-        gpio_pin_toggle_dt(&led);
-        k_msleep(SLEEP_TIME_MS);
+      // Toggle the LED basis the current status of USER button
+      bool val = gpio_pin_get_dt(&button);
+      // Set the GPIO value as per determined value of USER button
+      gpio_pin_set_dt(&led1, val);
+      // Keep toggeling the other led
+      gpio_pin_toggle_dt(&led0);
+      // Put the thread to sleep
+      k_msleep(SLEEP_TIME_MS);
     }
 
    return 0;
